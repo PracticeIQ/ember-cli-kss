bb5bb7b4bf26a2050ee1113e6c26fafdc428a61c
Tamara Chahine <tamara@balancedlabs.com>
2015-03-27 15:49:44 +1100

    Update glob dependency

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,7 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
 
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
@@ -16,7 +17,6 @@ var app = new EmberAddon();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-
 
 
 
