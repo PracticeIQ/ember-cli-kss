e70ec8c51885110e22c5e05d942bf9111168b35f
Tamara Chahine <tamara@balancedlabs.com>
2015-03-27 11:42:34 +1100

    Update output directories for catalogue templates, fix version number for npm

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+
+
+
 module.exports = app.toTree();
